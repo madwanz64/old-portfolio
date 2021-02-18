fda9b87adcd3a3eee5e3d84fc487cd6af74cc620
madwanz64 <madwanz64@gmail.com>
2021-02-18 14:37:53 +0700

    Update skill section and javacript

---
+++ assets/js/skill.js
@@ -0,0 +1,35 @@
+function showgd() {
+    document.getElementById("showgd").style.display = "block";
+    document.getElementById("hidegd").style.display = "none";
+    document.getElementById("gd").style.height = "100%";
+}
+
+function hidegd() {
+    document.getElementById("hidegd").style.display = "block";
+    document.getElementById("showgd").style.display = "none";
+    document.getElementById("gd").style.height = "0";
+}
+
+function showfe() {
+    document.getElementById("showfe").style.display = "block";
+    document.getElementById("hidefe").style.display = "none";
+    document.getElementById("fe").style.height = "100%";
+}
+
+function hidefe() {
+    document.getElementById("hidefe").style.display = "block";
+    document.getElementById("showfe").style.display = "none";
+    document.getElementById("fe").style.height = "0";
+}
+
+function showbe() {
+    document.getElementById("showbe").style.display = "block";
+    document.getElementById("hidebe").style.display = "none";
+    document.getElementById("be").style.height = "100%";
+}
+
+function hidebe() {
+    document.getElementById("hidebe").style.display = "block";
+    document.getElementById("showbe").style.display = "none";
+    document.getElementById("be").style.height = "0";
+}

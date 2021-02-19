d75e8a96186ee2e88fed01ce8c608d0d77ae9956
madwanz64 <madwanz64@gmail.com>
2021-02-19 07:34:22 +0700

    Finalizing skill panel and adding language

--- assets/js/skill.js
+++ assets/js/skill.js
@@ -1,35 +1,35 @@
 function showgd() {
     document.getElementById("showgd").style.display = "block";
     document.getElementById("hidegd").style.display = "none";
-    document.getElementById("gd").style.height = "100%";
+    document.getElementById("gd").classList.add("show");
 }
 
 function hidegd() {
     document.getElementById("hidegd").style.display = "block";
     document.getElementById("showgd").style.display = "none";
-    document.getElementById("gd").style.height = "0";
+    document.getElementById("gd").classList.remove("show");
 }
 
 function showfe() {
     document.getElementById("showfe").style.display = "block";
     document.getElementById("hidefe").style.display = "none";
-    document.getElementById("fe").style.height = "100%";
+    document.getElementById("fe").classList.add("show");
 }
 
 function hidefe() {
     document.getElementById("hidefe").style.display = "block";
     document.getElementById("showfe").style.display = "none";
-    document.getElementById("fe").style.height = "0";
+    document.getElementById("fe").classList.remove("show");
 }
 
 function showbe() {
     document.getElementById("showbe").style.display = "block";
     document.getElementById("hidebe").style.display = "none";
-    document.getElementById("be").style.height = "100%";
+    document.getElementById("be").classList.add("show");
 }
 
 function hidebe() {
     document.getElementById("hidebe").style.display = "block";
     document.getElementById("showbe").style.display = "none";
-    document.getElementById("be").style.height = "0";
+    document.getElementById("be").classList.remove("show");
 }
